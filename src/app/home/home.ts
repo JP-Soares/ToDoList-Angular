a6cfa21cb3469f7d0b8b5c719720d3edb4f9ab2d
JP <pedrojoao_costa@hotmail.com>
2025-11-01 12:45:55 -0300

    criando a pagina home e estilizando

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Menu } from '../menu/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   imports: [
     Menu,
-    MatIconModule
+    MatIconModule,
+    RouterLink
 ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from "@angular/router";
+import { MatIcon } from "@angular/material/icon";
 @Component({
   selector: 'app-menu',
   imports: [
-    MatToolbarModule
-  ],
+    MatToolbarModule,
+    RouterLink,
+    MatIcon
+],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
   standalone: true
